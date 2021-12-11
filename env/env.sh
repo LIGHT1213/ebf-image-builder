b2ce9b30b17aa8b9204cd5eb5463e4a6417f1a67
LIGHT1213 <35650324+LIGHT1213@users.noreply.github.com>
2021-12-12 00:27:47 +0800

    Create env.sh

---
+++ env/env.sh
@@ -0,0 +1,8 @@
+export FIRE_BOARD=ebf_imx_6ull_mini
+export LINUX=4.19.35
+export UBOOT=2020.10
+export DISTRIBUTION=Debian
+export DISTRIB_RELEASE=buster
+export DISTRIB_ARCH=armhf
+export DISTRIB_RELEASE=buster
+export INSTALL_TYPE=ALL
