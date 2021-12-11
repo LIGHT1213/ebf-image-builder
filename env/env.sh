15adda37f6ad245023085801cea5ee54084aa510
LIGHT1213 <35650324+LIGHT1213@users.noreply.github.com>
2021-12-12 00:39:22 +0800

    Update env.sh

--- env/env.sh
+++ env/env.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export FIRE_BOARD=ebf_imx_6ull_mini
 export LINUX=4.19.35
 export UBOOT=2020.10
@@ -6,3 +7,11 @@ export DISTRIB_RELEASE=buster
 export DISTRIB_ARCH=armhf
 export DISTRIB_RELEASE=buster
 export INSTALL_TYPE=ALL
+    FIRE_BOARD=ebf_imx_6ull_mini
+    LINUX=4.19.35
+    UBOOT=2020.10
+    DISTRIBUTION=Debian
+    DISTRIB_RELEASE=buster
+    DISTRIB_TYPE=console
+    INSTALL_TYPE=ALL
+    make  DOWNLOAD_MIRROR=china  FORCE_UPDATE=$rebuild
